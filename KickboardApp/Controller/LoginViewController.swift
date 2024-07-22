e43155cb901f826ea249372e014872874b242cbe
urdaeng <gni711@naver.com>
2024-07-22 21:03:34 +0900

    로그인 뷰

--- KickboardApp/Controller/LoginViewController.swift
+++ KickboardApp/Controller/LoginViewController.swift
@@ -6,24 +6,23 @@
 //
 
 import UIKit
+import SnapKit
 
 class LoginViewController: UIViewController {
+    
+//    private let loginView = LoginView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .white
+        
+//        view.addSubview(loginView)
+//        loginView.snp.makeConstraints {
+//            $0.edges.equalToSuperview()
+//        }
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+    
 
 }
