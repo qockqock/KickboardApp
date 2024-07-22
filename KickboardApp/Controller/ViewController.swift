3a57a9c7aa3296079a57b219355497a108f2d824
daesung <eogud8203@naver.com>
2024-07-22 12:24:18 +0900

    깃 이그노어 && 기본구성 세팅

---
+++ KickboardApp/Controller/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KickboardApp
+//
+//  Created by 머성이 on 7/22/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
