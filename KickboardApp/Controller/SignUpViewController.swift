36a457578a5f69b084a1fe26f9efd272c05ee0ef
shinsangkyu6660 <cherd3235@naver.com>
2024-07-23 16:00:48 +0900

    텍스트필드 - 텍스트 추가 페스워드 - 비밀번호 안보이게 하기 추가 , 동일한 비밀번호 나오게 추가 얼럿창 추가

--- KickboardApp/Controller/SignUpViewController.swift
+++ KickboardApp/Controller/SignUpViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 import SnapKit
 
-class SignUpViewController: UIViewController {
+class SignUpViewController: UIViewController, UITextFieldDelegate {
     private let signUpView = SignUpView()
     
     // MARK: - 뷰디드로드
@@ -18,6 +18,8 @@ class SignUpViewController: UIViewController {
         view.backgroundColor = .white
         signUpView.checkIdButton.addTarget(self, action: #selector(checkIdButtonTap), for: .touchDown)
         signUpView.membershipJoinButton.addTarget(self, action: #selector(membershipJoinButtonTap), for: .touchDown)
+        signUpView.userPassWordText.delegate = self
+        signUpView.userPassWordCheckText.delegate = self
     }
     
     // MARK: - 유저의 이메일을 확인하는란
@@ -45,7 +47,7 @@ class SignUpViewController: UIViewController {
             self.present(checkIdButtonTapAlert, animated: true, completion: nil)
             // 이메일 형식이 아닐때 뜨는 얼럿
         } else {
-            let nonEmailIdAlert = UIAlertController(title: "이메일 형식이 다릅니다", message: "이메일의 형식으로 다시 확인해 주세요", preferredStyle: .alert)
+            let nonEmailIdAlert = UIAlertController(title: "아이디 확인", message: (signUpView.userIdText.text?.isEmpty ?? true) ? "이메일 주소창이 비어 있습니다." : "이메일의 형식을 확인해주세요.", preferredStyle: .alert)
             print("이메일 형식이 다른 얼럿이 열렸습니다")
             nonEmailIdAlert.addAction(UIAlertAction(title: "확인", style: .destructive) { action in
                 print("확인 버튼이 클릭되었습니다")
@@ -71,9 +73,67 @@ class SignUpViewController: UIViewController {
         
         membershipAlert.addAction(UIAlertAction(title: "확인", style: .default) { action in
             print("확인 버튼이 클릭되었습니다")
-            
         })
         
         self.present(membershipAlert, animated: true, completion: nil)
     }
+    
+    // MARK: - 비밀번호 텍스트필드
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        if textField == signUpView.userPassWordText || textField == signUpView.userPassWordCheckText {
+            // 변경된 텍스트로 새 비밀번호를 생성
+            let newPassword = (textField.text as NSString?)?.replacingCharacters(in: range, with: string) ?? string
+            _ = userPassWordCheckPoint(password: newPassword)
+            let password1 = textField == signUpView.userPassWordText ? newPassword : signUpView.userPassWordText.text ?? ""
+            let password2 = textField == signUpView.userPassWordCheckText ? newPassword : signUpView.userPassWordCheckText.text ?? ""
+            checkPasswordsMatch(password1: password1, password2: password2)
+        }
+        return true
+    }
+    
+    // 비밀번호 체크 포인트 메서드
+    private func userPassWordCheckPoint(password: String) -> Bool {
+        if passwordCheck(password) {
+            print("비밀번호의 형식이 맞습니다")
+            signUpView.userPassWord.text = "사용 가능한 비밀번호 ✅"
+            print("정규표현식을 통과했습니다.")
+        } else {
+            signUpView.userPassWord.text = "비밀번호"
+        }
+        return true
+    }
+    
+    // 비밀번호 유효성 검사 메서드
+    private func passwordCheck(_ password: String) -> Bool {
+        print("비밀번호의 정규표현이 맞는지 확인합니다")
+        let passwordRegularExpression = "^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#\\$%\\^&\\*]).{8,}$" // 비밀번호 정규표현식 {8,}$ 가 8자리 이상을 표기함
+        let passwordtest = NSPredicate(format: "SELF MATCHES %@", passwordRegularExpression)
+        return passwordtest.evaluate(with: password)
+    }
+    
+    // MARK: - 비밀번호확인 텍스트필드
+    private func checkPasswordsMatch(password1: String, password2: String) {
+        if password1 == password2 {
+            signUpView.passWordCheck.text = "비밀번호가 일치합니다✅"
+            signUpView.passWordCheck.textColor = .blue // 비밀번호 일치 시 색상이 파란색으로 설정
+        } else {
+            signUpView.passWordCheck.text = "비밀번호를 확인해주세요!"
+            signUpView.passWordCheck.textColor = .red // 비밀번호 불일치 시 색상을 빨간색으로 설정
+        }
+    }
+    
+    // MARK: - 닉네임 텍스트 필드
+    private func userNickNameTextFiled() {
+        if signUpView.userNickNameText.text == nil || signUpView.userNickNameText.text == "" {
+            let nickNameAlert = UIAlertController(title: "닉네임을 입력해주세요", message: "닉네임이 비어있습니다 자신만의 특별한 닉네임을 선정해주세요.", preferredStyle: .alert)
+            print("닉네임 얼럿창이 열렸습니다.")
+            
+            nickNameAlert.addAction(UIAlertAction(title: "확인", style: .default) { action in
+                print("확인 버튼이 클릭되었습니다")
+            })
+            
+            self.present(nickNameAlert, animated: true, completion: nil)
+        }
+    }
+    
 }
