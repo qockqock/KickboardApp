817dc0de77e5626d6d5e47f81a50352f9e110614
kseunghui@gmail.com <kseunghui@gmail.com>
2024-07-23 15:09:28 +0900

    [feat] tabbar 구현

--- KickboardApp/Controller/MainTabbarController.swift
+++ KickboardApp/Controller/MainTabbarController.swift
@@ -1,5 +1,5 @@
 //
-//  MainTabbarControllerViewController.swift
+//  MainTabbarController.swift
 //  KickboardApp
 //
 //  Created by 김승희 on 7/23/24.
@@ -7,23 +7,30 @@
 
 import UIKit
 
-class MainTabbarControllerViewController: UITabBarController {
+class MainTabbarController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setupTabbar()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // Tabbar 생성하는 메서드 - sh
+    func setupTabbar() {
+        let firstTab = UINavigationController(rootViewController: ReturnViewController())
+        let secondTab = UINavigationController(rootViewController: MapViewController())
+        let thirdTab = UINavigationController(rootViewController: MyPageViewController())
+        
+        firstTab.tabBarItem = UITabBarItem(title: "반납하기", image: UIImage(named: "tabBarScooter"), tag: 0)
+        secondTab.tabBarItem = UITabBarItem(title: "지도", image: UIImage(named: "tabBarMap"), tag: 1)
+        thirdTab.tabBarItem = UITabBarItem(title: "마이페이지", image: UIImage(named: "tabBarMyPage"), tag: 2)
+        
+        viewControllers = [firstTab, secondTab, thirdTab]
+        
+        tabBar.barTintColor = .white
+        tabBar.tintColor = UIColor(red: 134/255, green: 74/255, blue: 238/255, alpha: 1.0)
+        tabBar.unselectedItemTintColor = .lightGray
+        tabBar.isTranslucent = false
+        
+        selectedIndex = 1
     }
-    */
-
 }
